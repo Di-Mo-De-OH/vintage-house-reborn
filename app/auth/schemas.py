b34c2a26580b2e6cbf9041e321501e1ca9e2f15d
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-07-09 17:20:00 +0900

    test: schemas 타입 추가

--- app/auth/schemas.py
+++ app/auth/schemas.py
@@ -1,4 +1,5 @@
 import re
+from typing import Self
 
 from pydantic import BaseModel, ConfigDict, Field, field_validator, model_validator
 
@@ -34,7 +35,7 @@ class SignUpRequest(BaseModel):
     verify_token: str = Field(examples=["Ab3dEfGhIjKlMnOpQrStUvWx1234"])
 
     @model_validator(mode="after")
-    def validate_confirm_password(self) -> str:
+    def validate_confirm_password(self) -> Self:
         if self.password != self.confirm_password:
             raise ValueError("비밀번호 및 확인 비밀번호가 일치하지 않습니다.")
         return self
