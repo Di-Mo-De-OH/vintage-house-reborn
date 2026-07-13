5685528018407caad6323772c4a8725f9c59fcdb
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-07-13 15:19:33 +0900

    style: 코드 포켓팅

--- alembic/versions/87af8e11070c_add_is_admin_to_users.py
+++ alembic/versions/87af8e11070c_add_is_admin_to_users.py
@@ -5,15 +5,12 @@ Revises: e79951599d68
 Create Date: 2026-07-13 06:16:30.918554
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
-revision: str = '87af8e11070c'
-down_revision: Union[str, Sequence[str], None] = 'e79951599d68'
+revision: str = "87af8e11070c"
+down_revision: Union[str, Sequence[str], None] = "e79951599d68"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
