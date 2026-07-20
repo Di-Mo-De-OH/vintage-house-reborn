f5b7fa37e2041133f2d0a1dc0128675c8e601c18
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-07-20 18:14:20 +0900

    feat: 장바구니 상품조회 읽기

--- app/cart/router.py
+++ app/cart/router.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter, Depends, status
 
 from app.auth.dependencies import get_current_user_id
 from app.cart.schemas.add import CartAddProductResponse
+from app.cart.schemas.read import CartResponse
 from app.cart.services.add import add
+from app.cart.services.read import get_cart_item
 from app.core.database import DbSession
 
 router = APIRouter(prefix="/cart", tags=["cart"])
@@ -13,3 +15,8 @@ async def add_cart_router(
     db: DbSession, product_id: str, user_id: str = Depends(get_current_user_id)
 ) -> CartAddProductResponse:
     return await add(db, product_id, user_id)
+
+
+@router.get("", status_code=status.HTTP_200_OK, response_model=CartResponse)
+async def get_cart_router(db: DbSession, user_id: str = Depends(get_current_user_id)) -> CartResponse:
+    return await get_cart_item(db, user_id)
